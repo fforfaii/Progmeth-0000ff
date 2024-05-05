fbb3fdff0b17d4df570395f50aeb5cf846213b42
fforfaii <156507299+fforfaii@users.noreply.github.com>
2024-05-05 23:36:40 +0700

    update CaveMapPane - can walk

--- src/gui/MapPane.java
+++ src/gui/MapPane.java
@@ -1,5 +1,7 @@
 package gui;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
@@ -9,9 +11,16 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import main.Main;
+
+import java.io.IOException;
 
 public class MapPane extends VBox {
     Text welcome;
+    Button MapBTN1;
+    Button MapBTN2;
+    Button MapBTN3;
+    Button MapBTN4;
     private static MapPane instance;
     public MapPane() {
         setBGImage();
@@ -32,6 +41,18 @@ public class MapPane extends VBox {
         VBox vBox4 = setMap("BG_forest2.jpg", "Map 4");
         hBox.getChildren().addAll(vBox1, vBox2, vBox3, vBox4);
 
+        // Change Scene to 'Each'MapPane
+        MapBTN1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                try {
+                    Main.getInstance().changeSceneJava(CaveMapPane.getInstance());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
         getChildren().addAll(welcome,hBox);
     }
 
@@ -48,6 +69,19 @@ public class MapPane extends VBox {
         mapText.setFont(Font.font("Monospace", FontWeight.BOLD,20));
         mapText.setTextFill(Color.rgb(0,51,102));
         mapText.setPrefWidth(150);
+        switch (mapName) {
+            case "Map 1":
+                MapBTN1 = mapText;
+                break;
+            case "Map 2":
+                MapBTN2 = mapText;
+            case "Map 3":
+                MapBTN3 = mapText;
+                break;
+            case "Map 4":
+                MapBTN4 = mapText;
+            default: break;
+        }
         vBox.getChildren().addAll(imageView,mapText);
 
         return vBox;
