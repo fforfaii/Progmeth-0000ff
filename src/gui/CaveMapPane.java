a68a4c53696044bc1bec1a7cdd1c6150a259f135
fforfaii <156507299+fforfaii@users.noreply.github.com>
2024-05-07 11:59:10 +0700

    resize Boom

--- src/gui/CaveMapPane.java
+++ src/gui/CaveMapPane.java
@@ -105,7 +105,6 @@ public class CaveMapPane extends AnchorPane {
                         }
                         canShoot = false;
                         Shoot(Gun);
-                        // Duration = 5
                         Timeline cooldownTimer = new Timeline(new KeyFrame(Duration.seconds(Punk.getInstance().getDelayShoot()), event -> canShoot = true));
                         cooldownTimer.play();
                         System.out.println("Boom!");
@@ -277,23 +276,19 @@ public class CaveMapPane extends AnchorPane {
 
         // Set Boom when Shoot
         Boom.setVisible(true);
+        Boom.setFitWidth(24);
+        Boom.setFitHeight(120);
         Boom.setLayoutX(mainChar.getLayoutX() + 22);
         System.out.println("start: " + Boom.getLayoutY());
 
         // Animate Boom moving upwards
-        TranslateTransition transition = new TranslateTransition(Duration.seconds(1), Boom);
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(0.5), Boom);
         transition.setByY(-453); // Move the Boom from its initial position (453) to 0
         transition.setOnFinished(event -> {
             // Hide Boom and reset its position after the animation finishes
             Boom.setVisible(false);
             Boom.setLayoutY(mainChar.getLayoutY());
 
-            TranslateTransition reverseTransition = new TranslateTransition(Duration.seconds(0), Boom); // No actual animation, just resetting the position
-//            reverseTransition.setToY(0); // Set Y position to 0 (initial position)
-//            reverseTransition.play();
-//            if (Boom.getLayoutY() - Minion.getLayoutY() <= 100 && Boom.getLayoutX() - Minion.getLayoutX() <= 100){
-//                Minion.setVisible(false);
-//            }
             System.out.println(Boom.getLayoutX()+", "+ Boom.getLayoutY());
             Boom.setTranslateY(0);
         });
